d339a00214708926d10aaba8557efc80f0c8e785
Fabio Ansaldi <fabio.ansaldi@bestvision.ch>
2022-12-14 17:39:05 +0100

    downgrade of node version

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import EnvironmentContext from "contexts/environmentContext";
 
 // eslint-disable-next-line no-unused-vars
 const Index = props => {
+    const { env } = useContext(EnvironmentContext);
+
     return (
-        <h1>ciao</h1>
+        <>
+            <h1>ciao</h1>
+            <p>
+                <small>PLATFORM</small>: <strong>{env.PLATFORM}</strong>
+            </p>
+        </>
     );
 };
 
