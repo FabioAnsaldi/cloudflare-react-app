bfe8f777c4e553bf7aaa7376cd464e845f37e575
Fabio Ansaldi <fabio.ansaldi@bestvision.ch>
2023-01-31 17:49:59 +0100

    next.config file added

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-undef */
+module.exports = {
+    images: {
+        loader: "custom", /* Image Optimization using Next.js' default loader is not compatible with `next export` */
+        path: ""
+    },
+};
